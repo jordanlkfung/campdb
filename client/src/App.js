bf72326f5ac63555b0c24c58edf7d2d8eadcdf6d
jordanlkfung <jordanlkfung@gmail.com>
2023-12-06 17:06:10 -0800

    inserts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
-
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{alert("Staff Username: john@example.com \nStaff Password:password1\n\nParent Username:parent2@example.com \nParent Password:password4")},[])
   return (
     <div class='flex flex-col items-center justify-center content-center h-screen'>
       <div class='flex-initial h-20 w-1/3 bg-sky-400 p-5 rounded-md text-center mt-6 flex items-center justify-center'><Link to='ParentSignIn'>Parent Login</Link></div>
--- mysqldb/create.sql
+++ mysqldb/create.sql
@@ -88,3 +88,81 @@ INNER JOIN CAMP ON CAMP_CAMPERS.camp_id = CAMP.id
 INNER JOIN PARENT_CHILDREN ON CHILD.id = PARENT_CHILDREN.child_id 
 INNER JOIN PARENT ON PARENT_CHILDREN.parent_email = PARENT.email;
 
+-- Inserting data into JOB_POSITIONS table
+INSERT INTO JOB_POSITIONS VALUES (1, 'Camp Counselor', 'Responsible for camper supervision', 15.00, 40);
+INSERT INTO JOB_POSITIONS VALUES (2, 'Activity Instructor', 'Teaches various camp activities', 20.00, 30);
+INSERT INTO JOB_POSITIONS VALUES (3, 'Camp Director', 'Oversees overall camp operations', 25.00, 45);
+INSERT INTO JOB_POSITIONS VALUES (4, 'Kitchen Staff', 'Prepares meals for campers and staff', 18.00, 35);
+INSERT INTO JOB_POSITIONS VALUES (5, 'Lifeguard', 'Ensures water safety at the camp lake', 18.50, 35);
+INSERT INTO JOB_POSITIONS VALUES (6, 'Art Instructor', 'Conducts creative art sessions for campers', 21.00, 25);
+INSERT INTO JOB_POSITIONS VALUES (7, 'Adventure Guide', 'Leads outdoor expeditions and activities', 22.00, 40);
+INSERT INTO JOB_POSITIONS VALUES (8, 'Camp Chef', 'Manages camp kitchen and meal preparations', 20.50, 35);
+
+-- Inserting data into EMPLOYEES table
+INSERT INTO EMPLOYEES VALUES ('John', 'Doe', '123 Main St', 'Cityville', 12345, 1, '555-1234', 'john@example.com', 'password1');
+INSERT INTO EMPLOYEES VALUES ('Jane', 'Smith', '456 Oak St', 'Townburg', 54321, 2, '555-5678', 'jane@example.com', 'password2');
+INSERT INTO EMPLOYEES VALUES ('Michael', 'Johnson', '789 Pine St', 'Forestville', 67890, 5, '555-3333', 'michael@example.com', 'password5');
+INSERT INTO EMPLOYEES VALUES ('Emily', 'Williams', '101 Maple St', 'Riverdale', 13579, 6, '555-4444', 'emily@example.com', 'password6');
+INSERT INTO EMPLOYEES VALUES ('Christopher', 'Brown', '222 Forest Ln', 'Woodsville', 54321, 7, '555-5555', 'christopher@example.com', 'password9');
+INSERT INTO EMPLOYEES VALUES ('Olivia', 'Taylor', '333 Meadow Dr', 'Greenville', 98765, 8, '555-6666', 'olivia@example.com', 'password10');
+
+-- Inserting data into PARENT table
+INSERT INTO PARENT VALUES ('parent1@example.com', 'Alice', 'Doe', 'password3', '555-1111');
+INSERT INTO PARENT VALUES ('parent2@example.com', 'Bob', 'Smith', 'password4', '555-2222');
+INSERT INTO PARENT VALUES ('parent3@example.com', 'Charlie', 'Wilson', 'password7', '555-3333');
+INSERT INTO PARENT VALUES ('parent4@example.com', 'Diana', 'Miller', 'password8', '555-4444');
+INSERT INTO PARENT VALUES ('parent5@example.com', 'Ethan', 'Davis', 'password11', '555-5555');
+INSERT INTO PARENT VALUES ('parent6@example.com', 'Sophia', 'Anderson', 'password12', '555-6666');
+
+-- Inserting data into CHILD table
+INSERT INTO CHILD VALUES (1, 'Child1', 'Doe');
+INSERT INTO CHILD VALUES (2, 'Child2', 'Smith');
+INSERT INTO CHILD VALUES (3, 'Child3', 'Johnson');
+INSERT INTO CHILD VALUES (4, 'Child4', 'Williams');
+INSERT INTO CHILD VALUES (5, 'Child5', 'Brown');
+INSERT INTO CHILD VALUES (6, 'Child6', 'Taylor');
+
+-- Inserting data into PARENT_CHILDREN table
+INSERT INTO PARENT_CHILDREN VALUES ('parent1@example.com', 1);
+INSERT INTO PARENT_CHILDREN VALUES ('parent2@example.com', 2);
+INSERT INTO PARENT_CHILDREN VALUES ('parent3@example.com', 3);
+INSERT INTO PARENT_CHILDREN VALUES ('parent4@example.com', 4);
+INSERT INTO PARENT_CHILDREN VALUES ('parent5@example.com', 5);
+INSERT INTO PARENT_CHILDREN VALUES ('parent6@example.com', 6);
+
+-- Inserting data into CAMP_LOCATIONS table
+INSERT INTO CAMP_LOCATIONS VALUES (1, 'Lakeview Campground', 'Lake City', 'CA', 98765, '789 Lakeview Rd');
+INSERT INTO CAMP_LOCATIONS VALUES (2, 'Mountain Retreat', 'Mountainville', 'CO', 54321, '123 Mountain Rd');
+INSERT INTO CAMP_LOCATIONS VALUES (3, 'Beachside Retreat', 'Seaville', 'FL', 54321, '456 Beach Rd');
+INSERT INTO CAMP_LOCATIONS VALUES (4, 'Desert Oasis Camp', 'Sandtown', 'AZ', 98765, '789 Dune Rd');
+INSERT INTO CAMP_LOCATIONS VALUES (5, 'Mountain Peak Camp', 'Summitville', 'CO', 87654, '789 Summit Rd');
+INSERT INTO CAMP_LOCATIONS VALUES (6, 'Riverbank Retreat', 'Rivertown', 'NY', 23456, '123 River Rd');
+
+-- Inserting data into CAMP table
+INSERT INTO CAMP VALUES (1, 'Summer Adventure Camp', 3, 1, 100, '2023-06-15', '2023-08-15');
+INSERT INTO CAMP VALUES (2, 'Nature Explorer Camp', 2, 2, 75, '2023-07-01', '2023-07-15');
+INSERT INTO CAMP VALUES (3, 'Water Adventure Camp', 4, 3, 80, '2023-07-10', '2023-07-25');
+INSERT INTO CAMP VALUES (4, 'Desert Explorer Camp', 5, 4, 60, '2023-08-01', '2023-08-15');
+INSERT INTO CAMP VALUES (5, 'Mountain Challenge Camp', 8, 5, 70, '2023-07-05', '2023-07-20');
+INSERT INTO CAMP VALUES (6, 'River Adventure Camp', 9, 6, 50, '2023-08-05', '2023-08-20');
+
+-- Inserting data into CAMP_CAMPERS table
+INSERT INTO CAMP_CAMPERS VALUES (1, 1);
+INSERT INTO CAMP_CAMPERS VALUES (2, 1);
+INSERT INTO CAMP_CAMPERS VALUES (1, 2);
+INSERT INTO CAMP_CAMPERS VALUES (3, 3);
+INSERT INTO CAMP_CAMPERS VALUES (4, 4);
+INSERT INTO CAMP_CAMPERS VALUES (5, 5);
+INSERT INTO CAMP_CAMPERS VALUES (6, 6);
+
+-- Inserting data into CAMP_STAFF table
+INSERT INTO CAMP_STAFF VALUES ('john@example.com', 1);
+INSERT INTO CAMP_STAFF VALUES ('jane@example.com', 2);
+INSERT INTO CAMP_STAFF VALUES ('michael@example.com', 3);
+INSERT INTO CAMP_STAFF VALUES ('emily@example.com', 4);
+INSERT INTO CAMP_STAFF VALUES ('christopher@example.com', 5);
+INSERT INTO CAMP_STAFF VALUES ('olivia@example.com', 6);
+INSERT INTO CAMP_STAFF VALUES ('christopher@example.com', 7);
+INSERT INTO CAMP_STAFF VALUES ('olivia@example.com', 8);
+INSERT INTO CAMP_STAFF VALUES ('christopher@example.com', 5);
+INSERT INTO CAMP_STAFF VALUES ('olivia@example.com', 6);
